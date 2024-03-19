7ae007ef71709d215ca302bf49f0673a1eca49e6
Claire B <158786976+Claireb123@users.noreply.github.com>
2024-03-19 00:52:57 -0500

    Add files via upload

---
+++ coffeeShop.js
@@ -0,0 +1,11 @@
+function menu() {
+    window.open('http://127.0.0.1:5500/menu.html');
+}
+
+function about() {
+    window.open('http://127.0.0.1:5500/about.html');
+}
+
+function contact() {
+    window.open('http://127.0.0.1:5500/contact.html');
+}
